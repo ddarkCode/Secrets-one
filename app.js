8ed26289e9c5f1de1bd5deb4aa47277e6a444074
Nnamani Ugochukwu Frederick <royal.ugoh@gmail.com>
2022-06-27 06:40:28 +0100

    Add Level One Authentication and Authorization to the Secrets App

--- app.js
+++ app.js
@@ -1,27 +1,78 @@
 require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
-const mongoose = require('mongoose');
+const { Schema, model, connect } = require('mongoose');
 
 const app = express();
 
-const { PORT } = process.env;
+const { PORT, MONGO_URL } = process.env;
+
+connect(MONGO_URL, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Database started');
+  }
+});
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
+const userSchema = new Schema({
+  email: String,
+  password: String,
+});
+const User = model('User', userSchema);
+
 app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.get('/register', (req, res) => {
-  res.render('register');
-});
+app
+  .route('/register')
+  .get((req, res) => {
+    res.render('register');
+  })
+  .post((req, res) => {
+    const { username, password } = req.body;
+    const newUser = new User({
+      email: username,
+      password,
+    });
+    newUser.save((err, savedUser) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render('secrets');
+      }
+    });
+  });
 
-app.get('/login', (req, res) => {
-  res.render('login');
-});
+app
+  .route('/login')
+  .get((req, res) => {
+    res.render('login');
+  })
+  .post((req, res) => {
+    const { username, password } = req.body;
+
+    User.findOne({ email: username }, (err, foundUser) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (!foundUser) {
+          return res.redirect('login');
+        } else {
+          if (foundUser.password === password) {
+            return res.render('secrets');
+          } else {
+            return res.json({ message: 'Password do not match.' });
+          }
+        }
+      }
+    });
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
